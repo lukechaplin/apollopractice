c750b5c5e1778ca6ea6e69c7e670e84eba5ace98
lukechaplin <lukechaplin@gmail.com>
2022-12-23 11:13:10 +0000

    added new type and field for length property in track and module types deprecated other length property

--- server/src/schema.js
+++ server/src/schema.js
@@ -26,8 +26,8 @@ const typeDefs = gql`
 		author: Author!
 		"The track's main illustration to display in track card or track page detail"
 		thumbnail: String
-		"The track's approximate length to complete, in minutes"
-		length: Int
+		"The track's approximate length to complete, in seconds"
+		length: Int @deprecated(reason: "Use durationInSeconds")
 		"The number of modules this track contains"
 		modulesCount: Int
 		"The track's complete description, can be in Markdown format"
@@ -36,6 +36,8 @@ const typeDefs = gql`
 		numberOfViews: Int
 		"The track's complete array of Modules"
 		modules: [Module!]!
+		"The track's full duration, in seconds"
+		durationInSeconds: Int
 	}
 
 	"Author of a complete Track"
@@ -52,8 +54,10 @@ const typeDefs = gql`
 		id: ID!
 		"The Module's title"
 		title: String!
-		"The Module's length in minutes"
-		length: Int
+		"The track's approximate length to complete, in seconds"
+		length: Int @deprecated(reason: "Use durationInSeconds")
+		"The track's full duration, in seconds"
+		durationInSeconds: Int
 	}
 
 	"To increment the number of views for a track"
